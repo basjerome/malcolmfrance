68027d20e86a183fbd7c8cede6ac2fab4a4f2ebc
basjerome <basjerome@gmail.com>
2018-03-28 14:10:19 +0200

    MàJ

--- wp-content/themes/malcolmfrance/episode.php
+++ wp-content/themes/malcolmfrance/episode.php
@@ -45,55 +45,30 @@
 	</div>
 	<?php endif; ?>
 
+	<?php if( get_field('mf_episode_images') ): ?>
 	<div class="owl-container">
 		<button class="expand" type="button"><i class="fa fa-expand"></i></button>
 		<div id="slideshow" class="owl-carousel">
+			<?php while( have_rows('mf_episode_images') ): the_row();
+				$img = get_sub_field('mf_episode_image');
+			?>
 			<div class="item">
 				<div class="illustration">
-					<img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" />
-					<span class="copyright">&copy; 20th Century Fox</span>
+					<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['title']; ?>" class="img-responsive" />
+					<span class="copyright">&copy; <?php echo $img['alt']; ?></span>
 				</div>
 			</div>
-			<div class="item">
-				<div class="illustration">
-					<img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" />
-					<span class="copyright">&copy; 20th Century Fox</span>
-				</div>
-			</div>
-			<div class="item">
-				<div class="illustration">
-					<img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" />
-					<span class="copyright">&copy; 20th Century Fox</span>
-				</div>
-			</div>
-			<div class="item">
-				<div class="illustration">
-					<img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" />
-					<span class="copyright">&copy; 20th Century Fox</span>
-				</div>
-			</div>
-			<div class="item">
-				<div class="illustration">
-					<img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" />
-					<span class="copyright">&copy; 20th Century Fox</span>
-				</div>
-			</div>
-			<div class="item">
-				<div class="illustration">
-					<img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" />
-					<span class="copyright">&copy; 20th Century Fox</span>
-				</div>
-			</div>
+			<?php endwhile; ?>
 		</div>
 		<div id="thumbnail" class="owl-carousel">
-			<div class="item"><img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" /></div>
-			<div class="item"><img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" /></div>
-			<div class="item"><img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" /></div>
-			<div class="item"><img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" /></div>
-			<div class="item"><img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" /></div>
-			<div class="item"><img src="http://www.malcolm-france.com/v3/site/img/page/default.jpg" alt="" class="img-responsive" /></div>
+			<?php while( have_rows('mf_episode_images') ): the_row();
+				$img = get_sub_field('mf_episode_image');
+			?>
+			<div class="item"><img src="<?php echo $img['url']; ?>" alt="<?php echo $img['title']; ?>" class="img-responsive" /></div>
+			<?php endwhile; ?>
 		</div>
 	</div>
+	<?php endif; ?>
 
 	<section class="technical-sheet">
 		<dl class="row">
@@ -167,12 +142,12 @@
 
 	<section class="summary">
 		<h3 class="section-title"><span>Résumé</span></h3>
-		<p><strong class="label">Scène d'introduction</strong> <a href="#" title="">Lois</a> et <a href="#" title="">Hal</a> dorment paisiblement dans leur lit. Mais ils sont rapidement perturbés par les garçons qui comme à leur habitude font des bêtises. Cependant, aucun des deux parents n'est visiblement décidé à aller calmer les esprits, jusqu'à ce que <strong>Hal</strong> craque. En contrepartie, il exige à Lois d'être nue à son retour !</p>
-		<p itemprop="reviewBody">La classe de <a href="#" title="">Malcolm</a> organise un grand pique-nique auquel est convié toute les familles des "<em>Têtes d'ampoule</em>", au grand dam de <strong>Malcolm</strong> qui n'est pas enclin à faire son numéro. <a href="#" title="">Francis</a>, à la maison pour le week-end, lui promet de l'aider à s'échapper.<br>
-		Sur place, <strong>Lois</strong> rencontre <a href="#" title="">Dorene</a>, la présidente de l'association des parents d'élèves et la mère de <a href="#" title="">Dabney</a>. Très vite, son caractère autoritaire exaspère <strong>Lois</strong> et encourage les autres mères à se rebeller contre la dame.<br>
-		Pendant ce temps, <strong>Francis</strong> fait la connaissance d'une jeune fille et engage avec elle une relation express, condamnant <strong>Malcolm</strong> à rester et à faire son numéro. Il décide de s'occuper en manipulant quelques produits chimiques.<br>
-		De son côté, <a href="#" title="">Reese</a> part à la chasse aux "<em>Têtes d'ampoule</em>", constatant qu'il y a du "<em>gibier</em>" ! Mais le grand frère de l'une d'entre elles vient compromettre vient compromettre la traque qui avait bien commencée.<br>
-		Enfin, <strong>Hal</strong> décide de participer à la préparation du repas en s'improvisant cuisinier du barbecue, avec l'aide de <a href="#" title="">Dewey</a>. Mais il n'a pas prévu que la majorité des "<em>Têtes d'ampoule</em>" sont allergiques à la viande…</p>
+		<?php if( get_field('mf_episode_introductory') ): ?>
+		<p><strong class="label">Scène d'introduction</strong> <?php the_field('mf_episode_introductory'); ?></p>
+		<?php endif; ?>
+		<?php if( get_field('mf_episode_excerpt') ): ?>
+		<p itemprop="reviewBody"><?php the_field('mf_episode_excerpt'); ?></p>
+		<?php endif; ?>
 	</section>
 
 	<div itemprop="publisher" itemscope="" itemtype="http://schema.org/Organization">
@@ -253,76 +228,33 @@
 	<section class="anecdotes">
 		<h3 class="section-title"><span>Anecdotes</span></h3>
 		<ul>
-			<li>Cet épisode marque la première apparition de <strong>Dabney</strong> et <strong>Lloyd</strong>, deux "<em>Têtes d’ampoule</em>".</li>
-			<li>Une scène coupée au montage montre <strong>Kyle</strong> faire comme les autres "<em>Têtes d'ampoule</em>" son numéro pathétique sur scène. Le numéro terminé, on entend une seule personne applaudir, qui n'est pas <strong>Caroline</strong> son institutrice comme elle l'avait fait avec <strong>Dabney</strong>, mais <strong>Reese</strong>, menacé par le grand frère de <strong>Kyle</strong>.</li>
-			<li>À la fin de l'épisode, on peut deviner <strong>Francis</strong> faire un doigt d'honneur. Aux États-Unis, ce geste est banni et systématiquement censuré des programmes. Même ceux d'<em>Eminem</em> ou des acteurs de <em>Jackass</em> sont floutés ou cachés. Dans cet épisode, le geste de <strong>Francis</strong> est ainsi suggéré, camouflé par la casquette de <strong>Dewey</strong>.</li>
+			<?php while( have_rows('mf_episode_anecdotes') ): the_row();
+				$anecdote = get_sub_field('mf_episode_anecdote');
+			?>
+			<li><?php echo $anecdote; ?></li>
+			<?php endwhile; ?>
 		</ul>
 	</section>
 	<?php endif; ?>
 
-	<?php if( get_field('mf_episode_anecdotes') ): ?>
+	<?php if( get_field('mf_episode_replicas') ): ?>
 	<section class="quotes">
 		<h3 class="section-title"><span>Répliques cultes</span></h3>
 		<ul>
-
+			<?php while( have_rows('mf_episode_replicas') ): the_row();
+				$line = get_sub_field('mf_episode_replicas_line');
+			?>
 			<li>
 				<ul>
-					<li><strong>Hal</strong> : T’as intérêt à être toute nue quand je reviendrais.</li>
-					<li><strong>Lois</strong> : D’accord.</li>
+					<?php while( have_rows('mf_episode_replicas_line') ): the_row();
+						$character = get_sub_field('mf_episode_replicas_character');
+						$dialogue = get_sub_field('mf_episode_replicas_dialogue');
+					?>
+					<li><strong><?php echo $character; ?></strong> : <?php echo $dialogue; ?></li>
+					<?php endwhile; ?>
 				</ul>
 			</li>
-
-			<li>
-				<ul>
-					<li><strong>Francis</strong> : Pourquoi il faut que je vienne au fait ?</li>
-					<li><strong>Lois</strong> : Parce que c’est une fête familiale, Francis, et que tu fais partie de la famille.</li>
-					<li><strong>Francis</strong> : Ah, oui, c’est vrai. J’ai un peu tendance à l’oublier à force de vivre à l’autre bout du pays dans mon école militaire.</li>
-				</ul>
-			</li>
-
-			<li>
-				<ul>
-					<li><strong>Reese</strong> : Un tout petit groupe de "Têtes d’ampoule" s’est écarté du reste du troupeau sans s’en rendre compte. Se retrouvant sans défense, ils deviennent des proies faciles pour les prédateurs. Ils sentent le danger, mais il est trop tard. Leur hésitation sera fatale.</li>
-				</ul>
-			</li>
-
-			<li>
-				<ul>
-					<li><strong>Dorene</strong> : Il semblerait que votre Malcolm possède un drôle de vocabulaire.</li>
-					<li><strong>Lois</strong> : Oh, il s’arrête pas. C’est bla bla bla, bla bla bla, à longueur de temps.</li>
-					<li><strong>Dorene</strong> : Oui, enfin, ce n’est pas de ce vocabulaire là dont je parlais. Il a apprit à quelques camarades de classe le mot qui commence par un M.</li>
-					<li><strong>Lois</strong> : Par un M ?</li>
-					<li><strong>Dorene</strong> : Il l’a sûrement entendu prononcé par quelqu’un dans la rue. Pas chez lui.</li>
-					<li><strong>Lois</strong> : Ouais, eh ben, je te dis M, vielle cloche.</li>
-				</ul>
-			</li>
-
-			<li>
-				<ul>
-					<li><strong>Homme</strong> : Il me faut deux hot-dogs.</li>
-					<li><strong>Hal</strong> : C’est parti, mon Dewey !</li>
-				</ul>
-			</li>
-
-			<li>
-				<ul>
-					<li><strong>Malcolm</strong> : Vous savez, dans de mauvaises mains, tous ces produits font une boule puante de première. Elles sont assez mauvaises pour ça !</li>
-				</ul>
-			</li>
-
-			<li>
-				<ul>
-					<li><strong>Malcolm</strong> : Ok. La différence entre une boule puante et une bombe chimique de première catégorie est apparemment de trois goûtes de sulfure tétra oxydé. Je vais faire un procès à ce site Internet.</li>
-				</ul>
-			</li>
-
-			<li>
-				<ul>
-					<li><strong>Dewey</strong> : Est-ce que Malcolm est un robot ?</li>
-					<li><strong>Hal</strong> : Non, fiston. Il est seulement intelligent. Vraiment, vraiment, vraiment intelligent. Mais, vraiment.</li>
-				</ul>
-			</li>
-
+			<?php endwhile; ?>
 		</ul>
 	</section>
 	<?php endif; ?>
@@ -358,41 +290,15 @@
 	<section class="gifs">
 		<h3 class="section-title"><span>Gifs</span></h3>
 		<div class="row">
+			<?php while( have_rows('mf_episode_gifs') ): the_row();
+				$gif = get_sub_field('mf_episode_gif');
+			?>
 			<div class="col-xs-6 col-md-4">
 				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-01.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-01.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-01.gif">
+					<a href="https://giphy.com/gifs/<?php echo $gif; ?>" target="_blank" title=""><img src="https://i.giphy.com/media/<?php echo $gif; ?>/giphy.webp" alt="" class="img-responsive" /></a>
 				</div>
 			</div>
-			<div class="col-xs-6 col-md-4">
-				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-02.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-02.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-02.gif">
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-4">
-				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-03.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-03.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-francis-03.gif">
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-4">
-				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-dewey-01.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-dewey-01.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-dewey-01.gif">
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-4">
-				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-hal-01.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-hal-01.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-hal-01.gif">
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-4">
-				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-hal-02.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-hal-02.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-hal-02.gif">
-				</div>
-			</div>
-			<div class="col-xs-6 col-md-4">
-				<div class="embed-responsive embed-responsive-16by9">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-enfants-01.jpg" alt="" class="img-responsive" data-static="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-enfants-01.jpg" data-animated="<?php echo get_template_directory_uri(); ?>/img/gif/s01e08-enfants-01.gif">
-				</div>
-			</div>
+			<?php endwhile; ?>
 		</div>
 	</section>
 	<?php endif; ?>
@@ -401,26 +307,21 @@
 	<section class="music">
 		<h3 class="section-title"><span>Musiques</span></h3>
 		<ul>
+			<?php while( have_rows('mf_episode_music') ): the_row();
+				$track = get_sub_field('mf_episode_music_track');
+				$scene = get_sub_field('mf_episode_music_scene');
+			?>
 			<li>
 				<ul>
 					<li>
-						<span class="label">Musique</span> "Nice Is Good, Mean Is Bad" - They Might Be Giants (Instrumental)
+						<span class="label">Musique</span> <?php echo $track; ?>
 					</li>
 					<li>
-						<span class="label">Scène</span> Caroline rencontre toute la famille de Malcolm.
+						<span class="label">Scène</span> <?php echo $scene; ?>
 					</li>
 				</ul>
 			</li>
-			<li>
-				<ul>
-					<li>
-						<span class="label">Musique</span> "Monster" - They Might Be Giants
-					</li>
-					<li>
-						<span class="label">Scène</span> Une panique totale a lieu ! Lois sème le trouble entre les mères de famille, Hal empoisonne les enfants, et Malcolm provoque une réaction chimique.
-					</li>
-				</ul>
-			</li>
+			<?php endwhile; ?>
 		</ul>
 	</section>
 	<?php endif; ?>
@@ -429,24 +330,14 @@
 	<section class="errors">
 		<h3 class="section-title"><span>Erreurs &amp; Faux raccords</span></h3>
 		<ul>
+			<?php while( have_rows('mf_episode_errors') ): the_row();
+				$timecode = get_sub_field('mf_episode_errors_timecode');
+				$description = get_sub_field('mf_episode_errors_description');
+			?>
 			<li>
-				<span class="label">01 min 10 s</span> Le badge que pose Malcolm à côté des tubes à essai de Stevie change de position lorsque ce dernier fait demi-tour avec son fauteuil.
+				<span class="label"><?php echo $timecode; ?></span> <?php echo $description; ?>
 			</li>
-			<li>
-				<span class="label">01 min 10 s</span> Quand Hal montre la viande dans sa glacière, les saucisses sont à droite et les steacks à gauche. Mais lorsque Dewey prend un steack pour son père, c'est l'inverse.
-			</li>
-			<li>
-				<span class="label">01 min 10 s</span> Malcolm apporte un hamburger à Caroline, et celle-ci le mange aux trois quarts. Quand Malcolm s'apprête à partir, le hamburger est à peine entamé.
-			</li>
-			<li>
-				<span class="label">01 min 10 s</span> Quand Stevie prépare son projet, son fauteuil roulant est accroché à la table. Puis, la corde disparaît.
-			</li>
-			<li>
-				<span class="label">01 min 10 s</span> Dans 2.25 – "Souvenirs, souvenirs" (Flashback), on apprend que Lois est allergique aux cacahuètes. Mais dans cet épisode, elle a préparé des cookies qui en contiennent.
-			</li>
-			<li>
-				<span class="label">01 min 10 s</span> On peut apercevoir plusieurs cadreurs pendant l'épisode. Quand les têtes d'ampoule attendent à la table, un cadreur est sous la table. Après l'explosion, l'un d'entre eux court après Malcolm.
-			</li>
+			<?php endwhile; ?>
 		</ul>
 	</section>
 	<?php endif; ?>
